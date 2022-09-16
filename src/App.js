2c1f3025343fbcd08f8016d9af1936c0de09e851
awais512 <raza.awais25@gmail.com>
2022-09-16 21:20:58 +0500

    Contact us page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Header, Home, Footer } from "./components";
+import { Header, Home, Footer, Contact } from "./components";
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/Founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
+import "./styles/contact.scss";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from "./Layout/Header";
 export { default as Founder } from "./Home/Founder";
 export { default as Menu } from "./Home/Menu";
 export { default as Footer } from "./Layout/Footer";
+export { default as Contact } from "./Contact/Contact";
