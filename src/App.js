f40a594cc693479c8d3e80beed67bd9b58ffb580
awais512 <raza.awais25@gmail.com>
2022-09-17 14:27:09 +0500

    Complete Ui

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import {
   Shipping,
   ConfirmOrder,
   PaymentSuccess,
+  About,
+  Login,
+  Profile,
+  MyOrders,
+  OrderDetails,
+  Dashboard,
+  Users,
+  Orders,
+  NotFound,
 } from "./components";
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -20,6 +29,12 @@ import "./styles/cart.scss";
 import "./styles/shipping.scss";
 import "./styles/confirmOrder.scss";
 import "./styles/paymentSuccess.scss";
+import "./styles/login.scss";
+import "./styles/profile.scss";
+import "./styles/table.scss";
+import "./styles/orderDetails.scss";
+import "./styles/dashboard.scss";
+import "./styles/about.scss";
 
 const App = () => {
   return (
@@ -32,6 +47,16 @@ const App = () => {
         <Route path="/shipping" element={<Shipping />} />
         <Route path="/confirmorder" element={<ConfirmOrder />} />
         <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/me" element={<Profile />} />
+        <Route path="/myorders" element={<MyOrders />} />
+        <Route path="/order/:id" element={<OrderDetails />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/admin/orders" element={<Orders />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
